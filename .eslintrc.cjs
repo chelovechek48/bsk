82b165e6ee7b2f65716271de77e1e93fface9daf
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-09-10 13:54:45 +0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:vue/strongly-recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  overrides: [{ files: ['**/*.js', '**/*.vue'] }],
+  plugins: ['import', 'vue'],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@images', './src/assets/images'],
+          ['@styles', './src/styles'],
+          ['@vars', './src/styles/vars'],
+          ['@json', './src/assets/json'],
+        ],
+      },
+    },
+  },
+};
---
+++ nuxt.config.js
@@ -0,0 +1,39 @@
+import path from 'path';
+
+const setAlias = (localPath) => path.resolve(__dirname, localPath);
+
+export default {
+  devtools: { enabled: false },
+  devServer: { port: '8080' },
+  compatibilityDate: '2024-09-09',
+
+  srcDir: './src',
+  app: {
+    baseURL: '/bsk',
+    head: {
+      title: 'Новости',
+      charset: 'UTF-8',
+      viewport: 'width=device-width, initial-scale=1.0',
+      htmlAttrs: { lang: 'ru' },
+    },
+  },
+
+  modules: ['nuxt-vitalizer'],
+  vitalizer: {
+    disableStylesheets: 'entry',
+  },
+
+  postcss: {
+    plugins: {
+      'postcss-preset-env': {},
+      'postcss-combine-media-query': {},
+    },
+  },
+
+  alias: {
+    '@images': setAlias('./src/assets/images'),
+    '@styles': setAlias('./src/styles'),
+    '@vars': setAlias('./src/styles/vars'),
+    '@json': setAlias('./src/assets/json'),
+  },
+};
